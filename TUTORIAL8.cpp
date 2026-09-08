59fab7a91b198ab30f20cb943ad5f53b30d14329
tanmaybeldar <tanmaybeldar2025.etc@mmcoe.edu.in>
2026-09-08 12:45:06 +0530

    Add try-catch for exception handling in division
    Implement exception handling for division by zero.

---
+++ TUTORIAL8.cpp
@@ -0,0 +1,21 @@
+//Write a C++ program which uses try and catch for exception handling
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a, b;
+    cout << "Enter two numbers: ";
+    cin >> a >> b;
+    try
+    {
+    if(b == 0)
+    throw b;
+    cout << "Result = " << a / b;
+    }
+    catch(int)
+    {
+    cout << "Exception: Cannot divide by zero";
+    }
+return 0;
+}
